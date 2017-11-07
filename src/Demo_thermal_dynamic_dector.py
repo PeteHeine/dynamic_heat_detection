b03845a166fbde0c01b32f7409b07d96697d66e2
PeteHeine <repetepc@gmail.com>
2017-11-07 22:38:52 +0100

    Pre-defence

--- src/Demo_thermal_dynamic_dector.py
+++ src/Demo_thermal_dynamic_dector.py
@@ -13,7 +13,7 @@ imgTemperature = np.load('/home/pistol/thermal_raw.npy')
 
 rSky = 0.2 # Area not to be used in the in determining the median temperature. 
 degAboveMedian = 3.0 # 
-degDiff = 15.0
+degDiff = 30.0
 rot90ImageK = 2
 connectComponents = True
 
@@ -29,13 +29,34 @@ print "Processing time: ", (time.time()-t0)*1000, "ms"
 imgTemperature = dd.ConvertFlirOutput2temperature(imgTemperature)
 imgTemperature = np.rot90(imgTemperature,k=2)
 
-
 imgTemperature[nRemoveSky:nRemoveSky+2,:] = np.max(imgTemperature)
+#plt.figure();
+#plt.imshow(imgTemperature)
+#plt.colorbar()
+#
+#
+#plt.figure();
+#plt.imshow(out)
+#plt.colorbar()
+
+
+## Figures for illutration 
 plt.figure();
 plt.imshow(imgTemperature)
 plt.colorbar()
 
+nRemoveSky = int(float(imgTemperature.shape[0])*rSky)
+
+# Determine median of area not being sky
+medianTemp = np.median(imgTemperature[nRemoveSky:,:])
+print "medianTemp: ", medianTemp
+
+imgTemperature2 = np.clip(imgTemperature-medianTemp-degAboveMedian,0.0,degDiff)
+
 plt.figure();
-plt.imshow(out)
+plt.imshow(imgTemperature2)
 plt.colorbar()
 
+plt.figure();
+plt.imshow(out.astype(np.float32)/255)
+plt.colorbar()
